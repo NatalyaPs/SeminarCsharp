045f1547d76fe846a12b5c82b6b7df59c9cea5e5
NatalyaPs <n09051@yandex.ru>
2022-08-14 18:52:32 +0300

    добавила новые задачи , решения и условия

--- task11/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿int number = new Random().Next(100, 1000);
-Console.WriteLine($"Наше случайное число = {number}");
-int numberA = number/100;
-int numberC = number % 10;
-Console.WriteLine(numberA*10 + numberC);
